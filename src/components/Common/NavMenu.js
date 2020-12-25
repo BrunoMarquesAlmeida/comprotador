d7374558d0fc7d2fb3f0786b18b4da267714151a
Bruno Almeida <bruno.almeida@hotmail.com>
2020-12-25 17:44:27 +0000

    Refactored NavMenu to a class based component

--- src/components/Common/NavMenu.js
+++ src/components/Common/NavMenu.js
@@ -1,63 +1,78 @@
+import React from "react";
 import { connect } from "react-redux";
 
 import { NavLink } from "react-router-dom";
 import { loadGAPI } from "./GoogleAuth";
 import { signIn, signOut } from "../../actions";
 
-const NavMenu = (props) => {
-  loadGAPI(props.signIn, props.signOut);
+class NavMenu extends React.Component {
+  componentDidMount() {
+    if (this.props.loginType === "Google") {
+      loadGAPI(this.props.signIn, this.props.signOut);
+    }
+  }
+  renderNavLinks = ({ subRoutes }) => {
+    return subRoutes.map(({ name, path, icon, exact }) => {
+      if (name === "Logout") {
+        return (
+          <span
+            onClick={this.handleLogoutClick}
+            key={name}
+            className="nav-link navMenuBtn"
+          >
+            {icon ? <i className={`fa ${icon}`}></i> : ""} {name}
+          </span>
+        );
+      } else {
+        return (
+          <NavLink
+            exact={exact}
+            to={path}
+            activeClassName="active"
+            className="nav-link"
+            key={name}
+          >
+            {icon ? <i className={`fa ${icon}`}></i> : ""} {name}
+          </NavLink>
+        );
+      }
+    });
+  };
+  handleLogoutClick = () => {
+    if (this.props.loginType === "Google") {
+      window.gapi.auth2.getAuthInstance().signOut();
+    } else if (this.props.loginType === "Facebook") {
+      window.FB.logout((response) => {
+        if (response.status === "unknown") {
+          this.props.signOut();
+        }
+      });
+    }
+  };
 
-  const { subRoutes, title } = props;
-  return (
-    <div className="col-lg-3">
-      <div className="card sidebar-menu mb-4">
-        <div className="card-header">
-          <h3 className="h4 card-title">{title}</h3>
-        </div>
-        <div className="card-body">
-          <ul className="nav nav-pills flex-column">
-            {renderNavLinks({ subRoutes })}
-          </ul>
+  render() {
+    const { subRoutes, title } = this.props;
+    return (
+      <div className="col-lg-3">
+        <div className="card sidebar-menu mb-4">
+          <div className="card-header">
+            <h3 className="h4 card-title">{title}</h3>
+          </div>
+          <div className="card-body">
+            <ul className="nav nav-pills flex-column">
+              {this.renderNavLinks({ subRoutes })}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-const handleLogoutClick = () => {
-  window.gapi.auth2.getAuthInstance().signOut();
-};
-
-const renderNavLinks = function ({ subRoutes }) {
-  return subRoutes.map(({ name, path, icon, exact }) => {
-    if (name === "Logout") {
-      return (
-        <span
-          onClick={handleLogoutClick}
-          key={name}
-          className="nav-link navMenuBtn"
-        >
-          {icon ? <i className={`fa ${icon}`}></i> : ""} {name}
-        </span>
-      );
-    } else {
-      return (
-        <NavLink
-          exact={exact}
-          to={path}
-          activeClassName="active"
-          className="nav-link"
-          key={name}
-        >
-          {icon ? <i className={`fa ${icon}`}></i> : ""} {name}
-        </NavLink>
-      );
-    }
-  });
-};
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
-  return { isSignedIn: state.auth.isSignedIn };
+  return {
+    loginType: state.auth.loginType,
+  };
 };
 
 export default connect(mapStateToProps, { signIn, signOut })(NavMenu);
