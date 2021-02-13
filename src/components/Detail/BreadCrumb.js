af358e9d2318a20e8b04a7d202b9c50b9aff1977
Bruno Almeida <bruno.almeida@hotmail.com>
2021-02-13 17:15:00 +0000

    Breadcrumb reveives and uses props from parent correctly

--- src/components/Detail/BreadCrumb.js
+++ src/components/Detail/BreadCrumb.js
@@ -1,27 +1,43 @@
 import { Link } from "react-router-dom";
 
-const BreadCrumb = () => {
-  return (
-    <div className="col-lg-12">
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="breadcrumb-item">
-            <Link to="/categoria/Computadores">Computadores</Link>
-          </li>
-          <li className="breadcrumb-item">
-            <Link to="/categoria/Computadores/Desktops">Desktops</Link>
-          </li>
-          <li className="breadcrumb-item">
-            <Link to="/categoria/Computadores/Desktops/Gaming">Gaming</Link>
-          </li>
-          <li className="breadcrumb-item active">Mini PC GMK NucBox</li>
-        </ol>
-      </nav>
-    </div>
-  );
+const BreadCrumb = (props) => {
+  if (props.fetchComplete !== undefined && props.fetchComplete !== null) {
+    const { categoria, subCategoria, subCategoria2 } = props.product.categorias;
+    const { title } = props.product;
+    return (
+      <div className="col-lg-12">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to="/categoria/Computadores">{categoria}</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to="/categoria/Computadores/Desktops">{subCategoria}</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link to="/categoria/Computadores/Desktops/Gaming">
+                {subCategoria2}
+              </Link>
+            </li>
+            <li className="breadcrumb-item active">{title}</li>
+          </ol>
+        </nav>
+      </div>
+    );
+  } else {
+    return (
+      <div className="col-lg-12">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item active">Loading...</li>
+          </ol>
+        </nav>
+      </div>
+    );
+  }
 };
 
 export default BreadCrumb;
