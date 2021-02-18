165c8574f662038a187e0d9750851fb0d991836d
Bruno Almeida <bruno.almeida@hotmail.com>
2021-02-18 19:15:55 +0000

    Added descriptive comments

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -13,40 +13,61 @@ class Category extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // what specs are selected for filtering
       specFiltersSelected: [],
+
+      // what page in the product list the user is on
       activePage: 1,
+
+      // how many products are displayed in the product list
       showN: 12,
+
+      // how the product list is sorted
       ordenarPor: "vendas",
     };
   }
 
+  // fetches all products in DB
+  componentDidMount() {
+    this.props.fetchAllProducts();
+  }
+
+  // handle sorting method chages
   handleSelectChange = ({ target }) => {
     this.setState({ ordenarPor: target.value, activePage: 1 });
   };
 
+  // sets showN to selected number
   showBtnClick = (n) => {
     this.setState({ showN: n, activePage: 1 });
   };
 
+  // handles page button clicks
   pageBtnClick = (i, numberOfPages) => {
     const next = this.state.activePage + 1;
     const prev = this.state.activePage - 1;
+    const userClickedNext = i === "»";
+    const userClickedPrev = i === "«";
 
-    if (i === "»" && next <= numberOfPages) {
+    if (userClickedNext) {
       this.setState({ activePage: next });
       return;
     }
-    if (i === "«" && prev >= 1) {
+    if (userClickedPrev) {
       this.setState({ activePage: prev });
       return;
     }
+
+    // user clicked number of page
     this.setState({ activePage: i });
   };
 
+  // user clicked 'remove all filters' button
   onRemoveFiltersClick = () => {
     this.setState({ specFiltersSelected: [], activePage: 1 });
   };
 
+  // user clicked on Filters Menu checkbox
   onFilterClick = (c) => {
     const { specFiltersSelected } = this.state;
     if (specFiltersSelected.includes(c)) {
@@ -62,10 +83,7 @@ class Category extends React.Component {
     }
   };
 
-  componentDidMount() {
-    this.props.fetchAllProducts();
-  }
-
+  // if (no subCategory is selected or no products are available) { we render a NavMenu } else { we render the FilterMenu}
   renderSideBar() {
     const { subCategoria } = this.props.match.params;
     const noResults = this.props.productsByCat.length === 0;
@@ -91,7 +109,6 @@ class Category extends React.Component {
         <div className="container">
           <div className="row">
             <BreadCrumb params={this.props.match.params} />
-
             {this.renderSideBar()}
             <ProductList
               specFiltersSelected={this.state.specFiltersSelected}
@@ -114,14 +131,20 @@ class Category extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const allProducts = Object.values(state.products);
   // const allProducts = db.produtos;
+  // local DB for testing purposes
+
+  const allProducts = Object.values(state.products);
   const { categoria, subCategoria, subCategoria2 } = ownProps.match.params;
+
   return {
     productsByCat: allProducts.filter((produto) => {
+      // first we filter out the fetchComplete
       const isNotFetchComplete =
         typeof produto === "object" && produto !== null;
+
       if (isNotFetchComplete) {
+        // then we filter products based on what categories the user navigated to
         const { categorias } = produto;
         if (subCategoria2) {
           return (
