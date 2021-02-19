cb7ce177ab56c16341c986f4894e2c07357cd340
Bruno Almeida <bruno.almeida@hotmail.com>
2021-02-19 19:52:43 +0000

    Added notification bar for products added to cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,6 +23,9 @@ class Header extends React.Component {
 
       // activeCat keeps track of what category the user is browsing through
       activeCat: "",
+
+      cartChange: null,
+      additionalProduct: null,
     };
     // loadGAPI to know if user is signed in through Google
     if (this.props.loginType === "Google") {
@@ -141,13 +144,20 @@ class Header extends React.Component {
     }
   }
 
-  // Where we test if the user is signed in through Auth0
-  componentDidUpdate() {
+  // Where we test if the user is signed in through Auth0 and if product was added to cart
+  componentDidUpdate(prevProps) {
     const { isAuthenticated } = this.props.auth0;
 
     if (isAuthenticated) {
       this.props.signIn(this.props.auth0.user.email, "Auth0");
     }
+
+    Object.values(this.props.shoppingCart).map((product) => {
+      const productWasAdded = prevProps.shoppingCart[product.id] === undefined;
+      if (productWasAdded) {
+        this.setState({ additionalProduct: product.title, cartChange: "add" });
+      }
+    });
   }
 
   // Handle submitting a search term
@@ -172,176 +182,212 @@ class Header extends React.Component {
   render() {
     const { isSignedIn } = this.props;
     const { navOpen, searchOpen } = this.state;
+    const cartItemAmount = Object.keys(this.props.shoppingCart).length;
 
     return (
-      <header className="header mb-5">
-        {/* Top bar */}
-        <div id="top">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-10 offer mb-3 mb-lg-0">
-                <Link to="/detalhes/5">
-                  <span className="btn btn-success btn-sm">
-                    Página de produto exibe informações
-                  </span>
-                  <span className="ml-1">
-                    Clique aqui para conhecer a última atualização ao site
-                  </span>
+      <>
+        <header className="header mb-5">
+          {/* Top bar */}
+          <div id="top">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-10 offer mb-3 mb-lg-0">
+                  <Link to="/detalhes/5">
+                    <span className="btn btn-success btn-sm">
+                      Página de produto exibe informações
+                    </span>
+                    <span className="ml-1">
+                      Clique aqui para conhecer a última atualização ao site
+                    </span>
+                  </Link>
+                </div>
+                <div className="col-lg-2 text-center text-lg-right">
+                  <ul className="menu list-inline mb-0">
+                    <li className="list-inline-item">
+                      {this.renderLoginBtn(isSignedIn)}
+                    </li>
+
+                    <li className="list-inline-item">
+                      <Link to="/info/contctos">Contactos</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Nav bar */}
+          <nav className="navbar navbar-expand-lg">
+            <div className="container">
+              {/* Logotipos */}
+              <Link to={"/"} className="navbar-brand home">
+                <img
+                  src="assets/img/logo.png"
+                  alt="ComproTador logo"
+                  className="d-none d-md-inline-block"
+                />
+                <img
+                  src="assets/img/logo-small.png"
+                  alt="ComproTador logo"
+                  className="d-inline-block d-md-none"
+                />
+                <span className="sr-only">ComproTador - Homepage</span>
+              </Link>
+              {/* Btn mobile */}
+              <div className="navbar-buttons">
+                <Button
+                  variant="secondary"
+                  type="button"
+                  className="btn btn-outline-secondary navbar-toggler"
+                  onClick={() => this.setState({ navOpen: !navOpen })}
+                  aria-controls="collapse-dropdown"
+                  aria-expanded={navOpen}
+                >
+                  <span className="sr-only">Toggle navigation</span>
+                  <i className="fa fa-align-justify"></i>
+                </Button>
+                <Button
+                  type="button"
+                  variant="secondary"
+                  className="btn btn-outline-secondary navbar-toggler"
+                  onClick={() => this.setState({ searchOpen: !searchOpen })}
+                  aria-controls="collapse-search"
+                  aria-expanded={searchOpen}
+                >
+                  <span className="sr-only">Toggle search</span>
+                  <i className="fa fa-search"></i>
+                </Button>
+                <Link
+                  to="/carrinho"
+                  className="btn btn-outline-secondary navbar-toggler"
+                >
+                  <i className="fa fa-shopping-cart"></i>
                 </Link>
               </div>
-              <div className="col-lg-2 text-center text-lg-right">
-                <ul className="menu list-inline mb-0">
-                  <li className="list-inline-item">
-                    {this.renderLoginBtn(isSignedIn)}
-                  </li>
 
-                  <li className="list-inline-item">
-                    <Link to="/info/contctos">Contactos</Link>
-                  </li>
-                </ul>
+              <Collapse in={this.state.navOpen}>
+                <div id="collapse-dropdown" className="navbar-collapse">
+                  {/* NavList */}
+                  <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                      <NavLink
+                        exact
+                        to="/"
+                        className="nav-link"
+                        activeClassName="active"
+                      >
+                        Home
+                      </NavLink>
+                    </li>
+                    {this.renderNavList()}
+                  </ul>
+                  {/* Btn desktop */}
+                  <div className="navbar-buttons d-flex justify-content-end">
+                    <div
+                      id="search-not-mobile"
+                      className="navbar-collapse collapse"
+                    ></div>
+                    <div
+                      id="basket-overview"
+                      className="navbar-collapse collapse d-none d-lg-block"
+                    >
+                      <Button
+                        className={`btn navbar-btn btn-primary d-none d-lg-inline-block`}
+                        onClick={() =>
+                          this.setState({ searchOpen: !searchOpen })
+                        }
+                        aria-controls="collapse-search"
+                        aria-expanded={searchOpen}
+                      >
+                        <span className="sr-only">Toggle search</span>
+                        <i className="fa fa-search"></i>
+                      </Button>
+                    </div>
+                    <div
+                      id="basket-overview"
+                      className="navbar-collapse collapse d-none d-lg-block"
+                    >
+                      <Link
+                        to="/carrinho"
+                        className="btn btn-primary navbar-btn"
+                      >
+                        <i className="fa fa-shopping-cart"></i>
+                        <span>{cartItemAmount} itens</span>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </Collapse>
+            </div>
+          </nav>
+          <Collapse in={this.state.searchOpen}>
+            <div id="collapse-search">
+              <div id="search">
+                <div className="container">
+                  <form
+                    role="search"
+                    className="ml-auto"
+                    onSubmit={this.onSearchSubmit}
+                  >
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        placeholder="Search"
+                        className="form-control"
+                        onChange={this.onSearchChange}
+                        value={this.state.searchTerm}
+                      />
+                      <div className="input-group-append">
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          onClick={this.onSearchSubmit}
+                        >
+                          <i className="fa fa-search"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </Collapse>
+        </header>
+        <div id="content" key={this.props.match.params}>
+          <div className="container">
+            <div className="row">
+              <div
+                className={
+                  this.state.cartChange !== null ? "col-lg-12" : "d-none"
+                }
+              >
+                <nav aria-label="breadcrumb">
+                  <ol className="breadcrumb text-muted">
+                    <div>
+                      <i
+                        className="fa fa-check-circle "
+                        style={{ color: "#3eaa94" }}
+                      ></i>{" "}
+                      Adicionou <b>{this.state.additionalProduct}</b>
+                      &nbsp;ao&nbsp;
+                      <Link to="/carrinho">Carrinho de Compras</Link>
+                    </div>
+                  </ol>
+                </nav>
               </div>
             </div>
           </div>
         </div>
-        {/* Nav bar */}
-        <nav className="navbar navbar-expand-lg">
-          <div className="container">
-            {/* Logotipos */}
-            <Link to={"/"} className="navbar-brand home">
-              <img
-                src="assets/img/logo.png"
-                alt="ComproTador logo"
-                className="d-none d-md-inline-block"
-              />
-              <img
-                src="assets/img/logo-small.png"
-                alt="ComproTador logo"
-                className="d-inline-block d-md-none"
-              />
-              <span className="sr-only">ComproTador - Homepage</span>
-            </Link>
-            {/* Btn mobile */}
-            <div className="navbar-buttons">
-              <Button
-                variant="secondary"
-                type="button"
-                className="btn btn-outline-secondary navbar-toggler"
-                onClick={() => this.setState({ navOpen: !navOpen })}
-                aria-controls="collapse-dropdown"
-                aria-expanded={navOpen}
-              >
-                <span className="sr-only">Toggle navigation</span>
-                <i className="fa fa-align-justify"></i>
-              </Button>
-              <Button
-                type="button"
-                variant="secondary"
-                className="btn btn-outline-secondary navbar-toggler"
-                onClick={() => this.setState({ searchOpen: !searchOpen })}
-                aria-controls="collapse-search"
-                aria-expanded={searchOpen}
-              >
-                <span className="sr-only">Toggle search</span>
-                <i className="fa fa-search"></i>
-              </Button>
-              <Link
-                to="/carrinho"
-                className="btn btn-outline-secondary navbar-toggler"
-              >
-                <i className="fa fa-shopping-cart"></i>
-              </Link>
-            </div>
-
-            <Collapse in={this.state.navOpen}>
-              <div id="collapse-dropdown" className="navbar-collapse">
-                {/* NavList */}
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item">
-                    <NavLink
-                      exact
-                      to="/"
-                      className="nav-link"
-                      activeClassName="active"
-                    >
-                      Home
-                    </NavLink>
-                  </li>
-                  {this.renderNavList()}
-                </ul>
-                {/* Btn desktop */}
-                <div className="navbar-buttons d-flex justify-content-end">
-                  <div
-                    id="search-not-mobile"
-                    className="navbar-collapse collapse"
-                  ></div>
-                  <div
-                    id="basket-overview"
-                    className="navbar-collapse collapse d-none d-lg-block"
-                  >
-                    <Button
-                      className={`btn navbar-btn btn-primary d-none d-lg-inline-block`}
-                      onClick={() => this.setState({ searchOpen: !searchOpen })}
-                      aria-controls="collapse-search"
-                      aria-expanded={searchOpen}
-                    >
-                      <span className="sr-only">Toggle search</span>
-                      <i className="fa fa-search"></i>
-                    </Button>
-                  </div>
-                  <div
-                    id="basket-overview"
-                    className="navbar-collapse collapse d-none d-lg-block"
-                  >
-                    <Link to="/carrinho" className="btn btn-primary navbar-btn">
-                      <i className="fa fa-shopping-cart"></i>
-                      <span>3 itens</span>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </Collapse>
-          </div>
-        </nav>
-        <Collapse in={this.state.searchOpen}>
-          <div id="collapse-search">
-            <div id="search">
-              <div className="container">
-                <form
-                  role="search"
-                  className="ml-auto"
-                  onSubmit={this.onSearchSubmit}
-                >
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      placeholder="Search"
-                      className="form-control"
-                      onChange={this.onSearchChange}
-                      value={this.state.searchTerm}
-                    />
-                    <div className="input-group-append">
-                      <button
-                        type="button"
-                        className="btn btn-primary"
-                        onClick={this.onSearchSubmit}
-                      >
-                        <i className="fa fa-search"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </Collapse>
-      </header>
+      </>
     );
   }
 }
 
 // Access to the Auth part of the Redux store, we need this to keep track of the user isSignedIn state through the app
 const mapStateToProps = (state) => {
-  return { isSignedIn: state.auth.isSignedIn };
+  return {
+    isSignedIn: state.auth.isSignedIn,
+    shoppingCart: state.shoppingCart,
+  };
 };
 
 // Exporting this withRouter allows us to use the history object outside of the Switch we have set up
