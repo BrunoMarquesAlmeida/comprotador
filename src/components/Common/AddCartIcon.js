1ca54c42799e6b7bd95c23458f00fe1c389f1a7e
Bruno Almeida <bruno.almeida@hotmail.com>
2021-02-19 19:53:11 +0000

    added a scroll to top of window

--- src/components/Common/AddCartIcon.js
+++ src/components/Common/AddCartIcon.js
@@ -1,7 +1,13 @@
 export const AddCartIcon = (props) => {
   const { addToCart, product } = props;
   return (
-    <span className="btn btn-primary" onClick={() => addToCart(product)}>
+    <span
+      className="btn btn-primary"
+      onClick={() => {
+        addToCart(product);
+        window.scrollTo(0, 0);
+      }}
+    >
       Adicionar ao
       <i className="fa fa-shopping-cart fa-icon" alt="carrinho"></i>
     </span>
