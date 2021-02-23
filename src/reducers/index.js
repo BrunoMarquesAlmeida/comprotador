4c236cf94509769b04079174cfddc75abe70e15a
Bruno Almeida <bruno.almeida@hotmail.com>
2021-02-23 19:51:15 +0000

    created orderReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import productsReducer from "./productsReducer";
 import shoppingCartReducer from "./shoppingCartReducer";
+import orderReducer from "./orderReducer";
 
 export default combineReducers({
   auth: authReducer,
   products: productsReducer,
   shoppingCart: shoppingCartReducer,
+  order: orderReducer,
 });
