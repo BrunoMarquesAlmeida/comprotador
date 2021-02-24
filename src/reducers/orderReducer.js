ba446d2ee3c5269b5aa47a8db19b6a94161e34ac
Bruno Almeida <bruno.almeida@hotmail.com>
2021-02-24 17:49:40 +0000

    ORDER_CHANGE reducer now stores state correctly

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -1,11 +1,17 @@
 import { ORDER_CHANGE } from "../actions";
 
-const INITIAL_STATE = { endereco: null };
+const INITIAL_STATE = { address: { firstName: "", lastName: "" } };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ORDER_CHANGE:
-      return action.payload;
+      return {
+        ...state,
+        [action.payload.checkoutStep]: {
+          ...state.address,
+          [action.payload.key]: action.payload.value,
+        },
+      };
     default:
       return state;
   }
