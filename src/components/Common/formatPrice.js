b4549b26feeaf334be5ff78f9e7c262e590d6330
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-05 11:56:21 +0000

    changed space to non-break space

--- src/components/Common/formatPrice.js
+++ src/components/Common/formatPrice.js
@@ -1,5 +1,5 @@
 const formatPrice = function (price) {
-  const formattedPrice = price.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
+  const formattedPrice = price.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1\u00a0");
   return formattedPrice;
 };
 
