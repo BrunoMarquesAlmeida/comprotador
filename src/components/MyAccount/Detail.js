931e5443566a82f031c661411114bfe82853e22b
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-11 18:14:59 +0000

    added user account address funcionality

--- src/components/MyAccount/Detail.js
+++ src/components/MyAccount/Detail.js
@@ -1,77 +1,83 @@
-const Detail = () => {
-  return (
-    <div className="col-lg-9">
-      <div className="box">
-        <h1>Detalhes</h1>
-        <p className="lead">Controle aqui a sua informação pessoal.</p>
-        <form>
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-group">
-                <label htmlFor="firstname">Primeiro nome</label>
-                <input id="firstname" type="text" className="form-control" />
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="form-group">
-                <label htmlFor="lastname">Último nome</label>
-                <input id="lastname" type="text" className="form-control" />
-              </div>
-            </div>
-          </div>
+import React from "react";
+import { Form, Field } from "react-final-form";
 
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-group">
-                <label htmlFor="company">Morada</label>
-                <input id="company" type="text" className="form-control" />
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-3">
-              <div className="form-group">
-                <label htmlFor="zip">Código postal</label>
-                <input id="zip" type="text" className="form-control" />
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-3">
-              <div className="form-group">
-                <label htmlFor="state">Cidade</label>
-                <input id="state" className="form-control" />
-              </div>
-            </div>
+class Detail extends React.Component {
+  renderInput(label, id) {
+    return (
+      <Field name={id}>
+        {({ meta, input }) => (
+          <div className="form-group">
+            <label>{label}</label>
+            <input {...input} id={id} type="text" className="form-control" />
+            {
+              // display validation errors
+              meta.error && meta.touched && (
+                <div className="invalid-feedback">{meta.error}</div>
+              )
+            }
           </div>
+        )}
+      </Field>
+    );
+  }
 
-          <div className="row">
-            <div className="col-md-6">
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input id="email" type="text" className="form-control" />
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-3">
-              <div className="form-group">
-                <label htmlFor="phone">Contacto telefónico</label>
-                <input id="phone" type="text" className="form-control" />
-              </div>
-            </div>
-            <div className="col-md-6 col-lg-3">
-              <div className="form-group">
-                <label htmlFor="state">Número de contribuinte (NIF)</label>
-                <input id="state" className="form-control" />
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <button type="submit" className="btn btn-primary">
-                <i className="fa fa-save"></i> Guardar alterações
-              </button>
-            </div>
-          </div>
-        </form>
+  render() {
+    const { address } = this.props;
+
+    return (
+      <div className="col-lg-9">
+        <div className="box">
+          <h1>Detalhes</h1>
+          <p className="lead">Controle aqui a sua informação pessoal.</p>
+          <Form
+            onSubmit={(values) => this.props.saveUserAddress(values)}
+            initialValues={address}
+            render={({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-md-6">
+                    {this.renderInput("Primeiro nome", "firstName")}
+                  </div>
+                  <div className="col-md-6">
+                    {this.renderInput("Último nome", "lastName")}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    {this.renderInput("Morada", "street")}
+                  </div>
+                  <div className="col-md-6 col-lg-3">
+                    {this.renderInput("Código postal", "zip")}
+                  </div>
+                  <div className="col-md-6 col-lg-3">
+                    {this.renderInput("Cidade", "city")}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    {this.renderInput("Email", "email")}
+                  </div>
+                  <div className="col-md-6 col-lg-3">
+                    {this.renderInput("Contacto telefónico", "phone")}
+                  </div>
+                  <div className="col-md-6 col-lg-3">
+                    {this.renderInput("NIF", "NIF")}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-12 text-center">
+                    <button className="btn btn-primary">
+                      <i className="fa fa-save"></i> Guardar alterações
+                    </button>
+                  </div>
+                </div>
+              </form>
+            )}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Detail;
