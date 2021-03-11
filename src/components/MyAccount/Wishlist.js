6984c2bf4cadc8238ea4193ded4ee1df62ee72a7
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-11 18:17:12 +0000

    passed props

--- src/components/MyAccount/Wishlist.js
+++ src/components/MyAccount/Wishlist.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { AddCartIcon } from "../Common/AddCartIcon";
 import { NewRibbon } from "../Common/Ribbon";
 
-const Wishlist = () => {
+const Wishlist = (props) => {
   return (
     <div className="col-lg-9">
       <div className="box">
