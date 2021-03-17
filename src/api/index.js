76ec1ae6bc239acd2acaa5bd408cef067f307172
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-17 18:26:03 +0000

    changed BaseURL

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "https://comprotador-api.herokuapp.com/",
-  // baseURL: "https://bma-ctapi.herokuapp.com/",
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://bma-ctapi.herokuapp.com/",
+  // baseURL: "http://localhost:3001/",
 });
