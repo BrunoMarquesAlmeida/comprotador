bf9c695e5b30b23990f1ba7dde6858f2bac20990
Bruno Almeida <bruno.almeida@hotmail.com>
2021-03-17 18:32:23 +0000

    created addToWishlistClick(), removeFromWishlistClick() and renderWishlistBtn(); refactor to class based component

--- src/components/Detail/ProductDetail.js
+++ src/components/Detail/ProductDetail.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ReactImageGallery from "react-image-gallery";
 import { Spinner } from "react-bootstrap";
@@ -8,104 +9,148 @@ import Recomendados from "../Common/Recomendados";
 import formatPrice from "../Common/formatPrice";
 
 // hosts enclosing JSX tags and tests whether to render that or a loading spinner
-const ProductDetail = (props) => {
-  // loading test
-  if (props.fetchComplete !== undefined && props.fetchComplete !== null) {
-    // result when loading is complete
-    const { product } = props;
-    const images = product.img;
-    const { novo, saldos } = product.ribbons;
+class ProductDetail extends React.Component {
+  // user clicked the 'Add to wishlist' button
+  addToWishlistClick() {
+    if (this.props.isSignedIn) {
+      this.props.addToWishlist(this.props.product._id);
+    } else {
+      this.props.handleLoginShow();
+    }
+  }
 
-    return (
-      <div className="col-lg-9 order-1 order-lg-2">
-        <div id="productMain" className="row">
-          <div className="col-md-6">
-            <div>
-              <ReactImageGallery
-                items={images}
-                thumbnailPosition="right"
-                showPlayButton={false}
-                infinite={false}
-                showNav={false}
-              />
-            </div>
-            {saldos ? <NewRibbon tipo="saldos" /> : null}
-            {novo ? <NewRibbon tipo="novo" /> : null}
-          </div>
-          <div className="col-md-6">
-            <div className="box">
-              <h1 className="text-center">{product.title}</h1>
+  // user clicked the 'Remove from wishlist' button
+  removeFromWishlistClick() {
+    this.props.removeFromWishlist(this.props.product._id);
+  }
 
-              <p className="price">
-                <del>
-                  {" "}
-                  {product.precos.desconto
-                    ? formatPrice(product.precos.normal)
-                    : ""}
-                </del>{" "}
-                {product.precos.desconto
-                  ? formatPrice(product.precos.desconto)
-                  : formatPrice(product.precos.normal)}
-                €
-              </p>
-              <p className="text-center buttons">
-                <AddCartIcon addToCart={props.addToCart} product={product} />
-                <span
-                  className="btn btn-outline-primary"
-                  onClick={() => props.addToWishlist(product._id)}
-                >
-                  Adicionar à wishlist
-                  <i className="fa fa-heart fa-icon"></i>
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div id="details" className="box">
-          <p></p>
-          {renderDescription(product.description)}
-          <br />
-          <h4>Especificações técnicas:</h4>
-          <ul>{renderSpecs(product.specs)}</ul>
-          <hr />
-          <div className="social">
-            <h4>Partilhar</h4>
-            <p>
-              <a className="external facebook">
-                <i className="fa fa-facebook"></i>
-              </a>
-              <a className="external gplus">
-                <i className="fa fa-google-plus"></i>
-              </a>
-              <a className="external twitter">
-                <i className="fa fa-twitter"></i>
-              </a>
-              <a className="email">
-                <i className="fa fa-envelope"></i>
-              </a>
-            </p>
-          </div>
-        </div>
-        <Recomendados />
-      </div>
+  // decides whether to render the 'Add' or 'Remove' from wishlist button based on if the product is already there
+  renderWishlistBtn() {
+    const itemIsInWishlist = this.props.wishlist.includes(
+      this.props.product._id
     );
-  } else {
-    // loading is not complete
+
+    if (itemIsInWishlist && this.props.isSignedIn) {
+      return (
+        <span
+          className="btn btn-outline-primary"
+          onClick={() => this.removeFromWishlistClick()}
+        >
+          Remover da wishlist
+          <i className="fa fa-heart fa-icon"></i>
+        </span>
+      );
+    }
     return (
-      <div className="col-lg-9 order-1 order-lg-2">
-        <div className="row h-100">
-          <div className="col-sm-12 my-auto">
-            <div className="mx-auto" style={{ width: "2rem" }}>
-              <Spinner animation="border" role="status" variant="info">
-                <span className="sr-only">Loading...</span>
-              </Spinner>
-            </div>
-          </div>
-        </div>
-      </div>
+      <span
+        className="btn btn-outline-primary"
+        onClick={() => this.addToWishlistClick()}
+      >
+        Adicionar à wishlist
+        <i className="fa fa-heart fa-icon"></i>
+      </span>
     );
   }
-};
+
+  render() {
+    // loading test
+    if (
+      this.props.fetchComplete !== undefined &&
+      this.props.fetchComplete !== null
+    ) {
+      // result when loading is complete
+      const { product } = this.props;
+      const images = product.img;
+      const { novo, saldos } = product.ribbons;
+
+      return (
+        <div className="col-lg-9 order-1 order-lg-2">
+          <div id="productMain" className="row">
+            <div className="col-md-6">
+              <div>
+                <ReactImageGallery
+                  items={images}
+                  thumbnailPosition="right"
+                  showPlayButton={false}
+                  infinite={false}
+                  showNav={false}
+                />
+              </div>
+              {saldos ? <NewRibbon tipo="saldos" /> : null}
+              {novo ? <NewRibbon tipo="novo" /> : null}
+            </div>
+            <div className="col-md-6">
+              <div className="box">
+                <h1 className="text-center">{product.title}</h1>
+
+                <p className="price">
+                  <del>
+                    {" "}
+                    {product.precos.desconto
+                      ? formatPrice(product.precos.normal)
+                      : ""}
+                  </del>{" "}
+                  {product.precos.desconto
+                    ? formatPrice(product.precos.desconto)
+                    : formatPrice(product.precos.normal)}
+                  €
+                </p>
+                <p className="text-center buttons">
+                  <AddCartIcon
+                    addToCart={this.props.addToCart}
+                    product={product}
+                  />
+                  {this.renderWishlistBtn()}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div id="details" className="box">
+            <p></p>
+            {renderDescription(product.description)}
+            <br />
+            <h4>Especificações técnicas:</h4>
+            <ul>{renderSpecs(product.specs)}</ul>
+            <hr />
+            <div className="social">
+              <h4>Partilhar</h4>
+              <p>
+                <a className="external facebook">
+                  <i className="fa fa-facebook"></i>
+                </a>
+                <a className="external gplus">
+                  <i className="fa fa-google-plus"></i>
+                </a>
+                <a className="external twitter">
+                  <i className="fa fa-twitter"></i>
+                </a>
+                <a className="email">
+                  <i className="fa fa-envelope"></i>
+                </a>
+              </p>
+            </div>
+          </div>
+          <Recomendados />
+        </div>
+      );
+    } else {
+      // loading is not complete
+      return (
+        <div className="col-lg-9 order-1 order-lg-2">
+          <div className="row h-100">
+            <div className="col-sm-12 my-auto">
+              <div className="mx-auto" style={{ width: "2rem" }}>
+                <Spinner animation="border" role="status" variant="info">
+                  <span className="sr-only">Loading...</span>
+                </Spinner>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
 // maps a products technical specifications unto JSX
 const renderSpecs = function (specs) {
